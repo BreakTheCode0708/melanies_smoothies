caa9e37807e6cf571be6e94ac9c261d25f88e05f
BreakTheCode0708 <139036106+BreakTheCode0708@users.noreply.github.com>
2024-09-26 19:13:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,10 +62,13 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 # Fetch the list of fruits from Snowflake
-fruit_options_df = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')).collect()
+fruit_options_df = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
+pd_df = fruit_options_df.to_pandas()
+#st.dataframe(pd_df)
 # Convert the Snowpark DataFrame to a list of fruit names
 fruit_options_list = [row['FRUIT_NAME'] for row in fruit_options_df]
+#st.stop()
 
 # Multiselect widget to choose fruits
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', fruit_options_list, max_selections=5)
@@ -97,7 +100,10 @@ if ingredients_list:
 
 if ingredients_list:
      for fruit_chosen in ingredients_list:
+         
          ingredients_string += fruit_chosen + ' '
+         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
          st.subheader(fruit_chosen + ' Nutrition Infromation')
          fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
          fv_df = st.dataframe(data= fruityvice_response.json() , use_container_width=True)
